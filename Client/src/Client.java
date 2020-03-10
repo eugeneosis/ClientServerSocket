efc4b7127665209dc85bbf9e1b812aabca376a02
eugeneosis <fckngbld75@gmail.com>
2020-03-10 16:01:03 +0300

    add logger

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -4,7 +4,7 @@ import java.net.Socket;
 public class Client {
     public static void main(String[] args) {
         try (
-                Socket socket = new Socket("127.0.0.1", 8000);
+                Socket socket = new Socket("127.0.0.1", 8080);
 
                 BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
                 BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,12 +1,16 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Logger;
 
 public class Server {
-    public static void main(String[] args) {
-        try (ServerSocket server = new ServerSocket(8000)) {
 
-            System.out.println("Server started!");
+    private final static Logger LOGGER = Logger.getLogger(Server.class.getName());
+
+    public static void main(String[] args) {
+        try (ServerSocket server = new ServerSocket(8080)) {
+
+            LOGGER.info("Server started");
 
             while (true) {
                 Socket socket = server.accept();
@@ -18,15 +22,18 @@ public class Server {
                         BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
                         String request = reader.readLine();
-                        System.out.println("Request : " + request);
+                        LOGGER.info("Received Request From Client");
+
                         String response = "Weather is : " + request.length() + " C";
                         Thread.sleep(3000);
-                        System.out.println("Response : " + response);
+
+                        LOGGER.info("Send Response To Client");
                         bufferedWriter.write(response);
                         bufferedWriter.newLine();
                         bufferedWriter.flush();
+                        LOGGER.info("Close Connection");
                     } catch (Exception e) {
-                        System.out.println("?");
+                        e.printStackTrace();
                     }
                 }).start();
             }
